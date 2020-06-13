c8034a8a014dc801bf34b4ce97124cc19b555356
Olga Glazunova <oaglazunova@gmail.com>
2020-06-13 10:52:26 +0300

    module2-task1 // proptypes added (#3)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {WelcomeScreen} from '../welcomeScreen/welcomeScreen.jsx';
+import PropTypes from "prop-types";
 
 export const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorCount} = props;
 
   return <WelcomeScreen errorCount={errorCount} />;
+};
+
+App.propTypes = {
+  errorCount: PropTypes.number,
 };
